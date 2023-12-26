2d2e1f2e68daf9b56c95c343d1b39ff8fb462107
JanAndrzejewski050 <janandrzejewski050@gmail.com>
2023-12-26 23:49:53 +0100

    dziala okno

--- szachy/main.py
+++ szachy/main.py
@@ -1,6 +1,6 @@
 import pygame as pg
 
-pygame.init()
+pg.init()
 
 width = 800
 height = 800
@@ -9,3 +9,18 @@ screen = pg.display.set_mode((width, height))
 pg.display.set_caption("Szachy")
 white = (255, 220, 178)
 brown = (205, 133, 63)
+
+
+def main():
+    pass
+
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+    screen.fill((0, 0, 0))
+
+    main()
+
+    pg.display.flip()
