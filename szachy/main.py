ee728d582063e88f70ce9a36715e40894f2ada62
Mateusz Matyskiel <114293147+Volqano@users.noreply.github.com>
2023-12-26 23:16:15 +0100

    Update main.py

--- szachy/main.py
+++ szachy/main.py
@@ -1,1 +1,11 @@
+import pygame as pg
 
+pygame.init()
+
+width = 800
+height = 800
+
+okno = pg.display.set_mode((width, height))
+pg.display.set_caption("Szachy")
+color1 = (255, 220, 178)
+color2 = (205, 133, 63)
