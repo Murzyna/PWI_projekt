64899c574d761b4cc3e5395f5d6922aea26b6d9e
Mateusz Matyskiel <114293147+Volqano@users.noreply.github.com>
2023-12-26 23:18:41 +0100

    Updated main.py
    nie robimy w ten sposob

--- szachy/main.py
+++ szachy/main.py
@@ -7,5 +7,5 @@ height = 800
 
 screen = pg.display.set_mode((width, height))
 pg.display.set_caption("Szachy")
-warm_white = (255, 220, 178)
-light_brown = (205, 133, 63)
+white = (255, 220, 178)
+brown = (205, 133, 63)
