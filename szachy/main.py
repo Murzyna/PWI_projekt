3b8f1410cb640fa95c352a4021cbec31c6ee821f
Jan Andrzejewski <150394043+JanAndrzejewski050@users.noreply.github.com>
2023-12-26 23:17:57 +0100

    Update main.py
    dodalem zmiany

--- szachy/main.py
+++ szachy/main.py
@@ -5,7 +5,7 @@ pygame.init()
 width = 800
 height = 800
 
-okno = pg.display.set_mode((width, height))
+screen = pg.display.set_mode((width, height))
 pg.display.set_caption("Szachy")
-color1 = (255, 220, 178)
-color2 = (205, 133, 63)
+warm_white = (255, 220, 178)
+light_brown = (205, 133, 63)
