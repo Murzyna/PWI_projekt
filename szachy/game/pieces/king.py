57a7e7ba7e7f924448f3face6d11e426e9408d7b
JanAndrzejewski050 <janandrzejewski050@gmail.com>
2023-12-27 15:38:23 +0100

    naprawka

--- szachy/game/pieces/king.py
+++ szachy/game/pieces/king.py
@@ -6,3 +6,6 @@ from PWI_projekt.szachy.game.base_piece import *
 class King(Piece):
     w_image = pg.image.load('assets/pieces/wK.png')
     b_image = pg.image.load('assets/pieces/bK.png')
+
+    def possible_moves_f(self, board):
+        pass
