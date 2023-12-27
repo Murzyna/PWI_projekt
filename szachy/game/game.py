acc2dad5df29e1b6c931622168741b47ccfb1381
Mateusz Matyskiel <114293147+Volqano@users.noreply.github.com>
2023-12-27 13:11:10 +0100

    Naprawilem blad pozdro

---
+++ szachy/game/game.py
@@ -0,0 +1,22 @@
+import pygame as pg
+import numpy as np
+
+clicked = False
+x = 0
+y = 0
+
+def highlight(screen):
+    color = (3, 166, 60)
+    global clicked
+    global x
+    global y
+    if pg.mouse.get_pressed()[0]:
+        cords = pg.mouse.get_pos()
+        if clicked == False:
+            x = np.floor(cords[0] / 100) * 100
+            y = np.floor(cords[1] / 100) * 100
+            target_rect = pg.draw.rect(screen, color, (x, y, 100, 100))
+            clicked = True
+            print("kutas")
+    if clicked == True:
+        target_rect = pg.draw.rect(screen, color, (x, y, 100, 100))
