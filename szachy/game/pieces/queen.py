d1e9cef92cbe5f2dd96c156d9b530bff5046719e
Volqano <theriverpl@gmail.com>
2023-12-27 15:17:22 +0100

    -Dodano możliwe ruchy dla Hetmana

--- szachy/game/pieces/queen.py
+++ szachy/game/pieces/queen.py
@@ -6,3 +6,100 @@ from PWI_projekt.szachy.game.base_piece import *
 class Queen(Piece):
     w_image = pg.image.load('assets/pieces/wQ.png')
     b_image = pg.image.load('assets/pieces/bQ.png')
+
+    def possible_moves_f(self, board):
+        index = 0
+        y = self.pos[0]
+        x = self.pos[1]
+        for i in range(x + 1, 8):
+            if board[y][i] == 0:
+                self.possible_moves[index] = (y, i)
+                index += 1
+            elif board[y][i] == self.color:
+                break
+            else:
+                self.possible_moves[index] = (y, i)
+                index += 1
+                break
+        for i in range(x - 1, -1, -1):
+            if board[y][i] == 0:
+                self.possible_moves[index] = (y, i)
+                index += 1
+            elif board[y][i] == self.color:
+                break
+            else:
+                self.possible_moves[index] = (y, i)
+                index += 1
+                break
+        for i in range(y + 1, 8):
+            if board[i][x] == 0:
+                self.possible_moves[index] = (i, x)
+                index += 1
+            elif board[i][x] == self.color:
+                break
+            else:
+                self.possible_moves[index] = (i, x)
+                index += 1
+                break
+        for i in range(y - 1, -1, -1):
+            if board[i][x] == 0:
+                self.possible_moves[index] = (i, x)
+                index += 1
+            elif board[i][x] == self.color:
+                break
+            else:
+                self.possible_moves[index] = (i, x)
+                index += 1
+                break
+        for i in range(y - 1, -1, -1):
+            j = x + 1
+            if j == 8:
+                break
+            if board[i][j] == 0:
+                self.possible_moves[index] = (i, j)
+                index += 1
+            elif board[i][j] == self.color:
+                break
+            else:
+                self.possible_moves[index] = (i, j)
+                index += 1
+                break
+        for i in range(y - 1, -1, -1):
+            j = x - 1
+            if j == -1:
+                break
+            if board[i][j] == 0:
+                self.possible_moves[index] = (i, j)
+                index += 1
+            elif board[i][j] == self.color:
+                break
+            else:
+                self.possible_moves[index] = (i, j)
+                index += 1
+                break
+        for i in range(y + 1, 8):
+            j = x + 1
+            if j == 8:
+                break
+            if board[i][j] == 0:
+                self.possible_moves[index] = (i, j)
+                index += 1
+            elif board[i][j] == self.color:
+                break
+            else:
+                self.possible_moves[index] = (i, j)
+                index += 1
+                break
+        for i in range(y + 1, 8):
+            j = x - 1
+            if j == 0:
+                break
+            if board[i][j] == 0:
+                self.possible_moves[index] = (i, j)
+                index += 1
+            elif board[i][j] == self.color:
+                break
+            else:
+                self.possible_moves[index] = (i, j)
+                index += 1
+                break
