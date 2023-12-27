64df581bec95b41236056661e255269382d78b89
JanAndrzejewski050 <janandrzejewski050@gmail.com>
2023-12-27 15:38:23 +0100

    gowno

--- szachy/game/pieces/pawn.py
+++ szachy/game/pieces/pawn.py
@@ -6,3 +6,6 @@ from PWI_projekt.szachy.game.base_piece import *
 class Pawn(Piece):
     w_image = pg.image.load('assets/pieces/wP.png')
     b_image = pg.image.load('assets/pieces/bP.png')
+
+    def possible_moves_f(self, board):
+        pass
