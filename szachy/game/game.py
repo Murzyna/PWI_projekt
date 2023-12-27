e4f15cc2831abcc640debff14a867afaa4d78335
Mateusz Matyskiel <114293147+Volqano@users.noreply.github.com>
2023-12-27 13:19:25 +0100

    usprawniono

--- szachy/game/game.py
+++ szachy/game/game.py
@@ -12,11 +12,11 @@ def highlight(screen):
     global y
     if pg.mouse.get_pressed()[0]:
         cords = pg.mouse.get_pos()
-        if clicked == False:
+        if clicked is False:
             x = np.floor(cords[0] / 100) * 100
             y = np.floor(cords[1] / 100) * 100
             target_rect = pg.draw.rect(screen, color, (x, y, 100, 100))
             clicked = True
             print("kutas")
-    if clicked == True:
+    if clicked is True:
         target_rect = pg.draw.rect(screen, color, (x, y, 100, 100))
