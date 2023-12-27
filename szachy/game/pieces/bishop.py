c6d3f35375f626537a2bb856b490e4b9e1682a5a
JanAndrzejewski050 <janandrzejewski050@gmail.com>
2023-12-27 21:41:47 +0100

    possible_moves_f dziala dla wszysekitgo pozdro

--- szachy/game/pieces/bishop.py
+++ szachy/game/pieces/bishop.py
@@ -8,21 +8,30 @@ class Bishop(Piece):
     b_image = pg.image.load('assets/pieces/bB.png')
 
     def possible_moves_f(self, board):
-        self.possible_moves = np.empty((0, 2), dtype=int)
         y, x = self.pos
 
         # Define the directions to check (diagonal)
         directions = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
 
+        max_possible_moves = 13  # Maksymalna liczba potencjalnych ruchów gońca
+        possible_moves_array = np.full((max_possible_moves, 2), -1, dtype=int)
+
+        count_moves = 0
+
         for dy, dx in directions:
             i, j = y + dy, x + dx
             while 0 <= i < 8 and 0 <= j < 8:
                 if board[i][j] == 0:
-                    self.possible_moves = np.append(self.possible_moves, np.array([[i, j]]), axis=0)
+                    possible_moves_array[count_moves] = [i, j]
+                    count_moves += 1
                 elif board[i][j].color == self.color:
                     break
                 else:
-                    self.possible_moves = np.append(self.possible_moves, np.array([[i, j]]), axis=0)
+                    possible_moves_array[count_moves] = [i, j]
+                    count_moves += 1
                     break
                 i += dy
                 j += dx
+
+        # Obcięcie tablicy do rzeczywistej liczby możliwych ruchów
+        self.possible_moves = possible_moves_array[:count_moves]
--- szachy/game/pieces/pawn.py
+++ szachy/game/pieces/pawn.py
@@ -6,29 +6,57 @@ from PWI_projekt.szachy.game.base_piece import *
 class Pawn(Piece):
     w_image = pg.image.load('assets/pieces/wP.png')
     b_image = pg.image.load('assets/pieces/bP.png')
-
+    
     def __init__(self, color, pos, size):
         super().__init__(color, pos, size)
         self.first_move = True
 
     def possible_moves_f(self, board):
-        self.possible_moves = np.empty((0, 2), dtype=int)
         y, x = self.pos
 
-        # Define the direction based on the color of the pawn
-        direction = 1 if self.color == 'w' else -1
+        max_possible_moves = 4  # Maksymalna liczba potencjalnych ruchów pionka
+        possible_moves_array = np.full((max_possible_moves, 2), -1, dtype=int)
 
-        # Move one square forward
-        new_y = y + direction
-        if 0 <= new_y < 8 and board[new_y][x] == 0:
-            self.possible_moves = np.append(self.possible_moves, np.array([[new_y, x]]), axis=0)
+        count_moves = 0
 
-            # Move two squares forward on the first move
-            if self.first_move and board[y + 2 * direction][x] == 0:
-                self.possible_moves = np.append(self.possible_moves, np.array([[y + 2 * direction, x]]), axis=0)
+        if self.color == "w":
+            if board[y-1][x] == 0:              # ruch do przodu
+                self.first_move = False
+                possible_moves_array[count_moves] = [y-1, x]
+                count_moves += 1
+            if 0<y<8 and 0<=x<8 and board[y-1][x+1] != 0 and board[y-1][x+1].color != self.color:       # bicie na ukos
+                self.first_move = False
+                possible_moves_array[count_moves] = [y-1, x+1]
+                count_moves += 1
+            if 0<y<8 and 0<x<=8 and board[y-1][x-1] != 0 and board[y-1][x-1].color != self.color:       # bicie na ukos
+                self.first_move = False
+                possible_moves_array[count_moves] = [y-1, x-1]
+                count_moves += 1
+            if self.first_move and board[y-2][x] == 0:
+                self.first_move = False
+                possible_moves_array[count_moves] = [y-2, x]
+                count_moves += 1
 
-        # Check diagonal moves for capturing
-        for dx in [-1, 1]:
-            new_x = x + dx
-            if 0 <= new_x < 8 and 0 <= new_y < 8 and board[new_y][new_x] != 0 and board[new_y][new_x].color != self.color:
-                self.possible_moves = np.append(self.possible_moves, np.array([[new_y, new_x]]), axis=0)
+        if self.color == "b":
+            if board[y+1][x] == 0:              # ruch do przodu
+                self.first_move = False
+                possible_moves_array[count_moves] = [y+1, x]
+                count_moves += 1
+
+            if 0<y<8 and 0<=x<8 and board[y+1][x+1] != 0 and board[y+1][x+1].color != self.color:       # bicie na ukos
+                self.first_move = False
+                possible_moves_array[count_moves] = [y+1, x+1]
+                count_moves += 1
+
+            if 0<y<8 and 0<x<=8 and board[y+1][x-1] != 0 and board[y+1][x-1].color != self.color:       # bicie na ukos
+                self.first_move = False
+                possible_moves_array[count_moves] = [y+1, x-1]
+                count_moves += 1
+
+            if self.first_move and board[y+2][x] == 0:
+                self.first_move = False
+                possible_moves_array[count_moves] = [y+2, x]
+                count_moves += 1
+
+        # Obcięcie tablicy do rzeczywistej liczby możliwych ruchów
+        self.possible_moves = possible_moves_array[:count_moves]
