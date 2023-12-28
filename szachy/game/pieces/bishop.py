b03459c7177a148d1e0c830e2ab11635706eee45
JanAndrzejewski050 <janandrzejewski050@gmail.com>
2023-12-28 13:33:01 +0100

    usprawnienie

--- szachy/game/pieces/bishop.py
+++ szachy/game/pieces/bishop.py
@@ -35,3 +35,31 @@ class Bishop(Piece):
 
         # Obcięcie tablicy do rzeczywistej liczby możliwych ruchów
         self.possible_moves = possible_moves_array[:count_moves]
+
+    def attacts(self, board):
+        y, x = self.pos
+
+        # Define the directions to check (diagonal)
+        directions = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
+
+        max_possible_moves = 13  # Maksymalna liczba potencjalnych ruchów gońca
+        possible_moves_array = np.full((max_possible_moves, 2), -1, dtype=int)
+
+        count_moves = 0
+
+        for dy, dx in directions:
+            i, j = y + dy, x + dx
+            while 0 <= i < 8 and 0 <= j < 8:
+                if board[i][j] == 0:
+                    possible_moves_array[count_moves] = [i, j]
+                    count_moves += 1
+
+                else:
+                    possible_moves_array[count_moves] = [i, j]
+                    count_moves += 1
+                    break
+                i += dy
+                j += dx
+
+        # Obcięcie tablicy do rzeczywistej liczby możliwych ruchów
+        self.possible_attacts = possible_moves_array[:count_moves]
