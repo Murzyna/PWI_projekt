fbf218746b05c91256cc1cfb551d228bb32551db
JanAndrzejewski050 <janandrzejewski050@gmail.com>
2023-12-28 12:20:26 +0100

    usprawnienie

--- szachy/game/base_piece.py
+++ szachy/game/base_piece.py
@@ -12,6 +12,7 @@ class Piece:
         self.size = size
         self.possible_moves = None
 
+
     def draw_piece(self, screen, w_image, b_image):
         w_scaled_image = pg.transform.scale(w_image, (self.size, self.size))
         b_scaled_image = pg.transform.scale(b_image, (self.size, self.size))
