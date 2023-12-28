5c67082f24c0b09f5f12944e85e61d92f27c27d9
JanAndrzejewski050 <janandrzejewski050@gmail.com>
2023-12-28 13:33:01 +0100

    usprawnienie

--- szachy/game/pieces/pawn.py
+++ szachy/game/pieces/pawn.py
@@ -60,3 +60,53 @@ class Pawn(Piece):
 
         # Obcięcie tablicy do rzeczywistej liczby możliwych ruchów
         self.possible_moves = possible_moves_array[:count_moves]
+
+
+    def attacts(self, board):
+        y, x = self.pos
+
+        max_possible_moves = 4  # Maksymalna liczba potencjalnych ruchów pionka
+        possible_moves_array = np.full((max_possible_moves, 2), -1, dtype=int)
+
+        count_moves = 0
+
+        if self.starting_pos != self.pos:
+            self.first_move = False
+
+        if self.color == "w":
+            if self.first_move and board[y-2][x] == 0:
+                possible_moves_array[count_moves] = [y-2, x]
+                count_moves += 1
+
+            if board[y-1][x] == 0:              # ruch do przodu
+                possible_moves_array[count_moves] = [y-1, x]
+                count_moves += 1
+
+            if 0<y<8 and 0<=x<7 and board[y-1][x+1]:       # bicie na ukos
+                possible_moves_array[count_moves] = [y-1, x+1]
+                count_moves += 1
+
+            if 0<y<8 and 1<x<=8 and board[y-1][x-1] != 0:       # bicie na ukos
+                possible_moves_array[count_moves] = [y-1, x-1]
+                count_moves += 1
+
+
+        if self.color == "b":
+            if self.first_move and board[y+2][x] == 0:
+                possible_moves_array[count_moves] = [y+2, x]
+                count_moves += 1
+
+            if board[y+1][x] == 0:              # ruch do przodu
+                possible_moves_array[count_moves] = [y+1, x]
+                count_moves += 1
+
+            if 0<y<8 and 0<=x<7 and board[y+1][x+1] != 0:       # bicie na ukos
+                possible_moves_array[count_moves] = [y+1, x+1]
+                count_moves += 1
+
+            if 0<y<8 and 1<x<=8 and board[y+1][x-1] != 0:       # bicie na ukos
+                possible_moves_array[count_moves] = [y+1, x-1]
+                count_moves += 1
+
+        # Obcięcie tablicy do rzeczywistej liczby możliwych ruchów
+        self.possible_attacts = possible_moves_array[:count_moves]
