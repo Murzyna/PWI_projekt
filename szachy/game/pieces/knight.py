56da5f25a29c138b6b3d9634e5fc0d87f8a031ef
JanAndrzejewski050 <janandrzejewski050@gmail.com>
2023-12-28 13:42:23 +0100

    usprawnienie

--- szachy/game/pieces/knight.py
+++ szachy/game/pieces/knight.py
@@ -40,7 +40,7 @@ class Knight(Piece):
 
         for i in range(8):
             new_x, new_y = x + rows[i], y + cols[i]
-            if 8 > new_x >= 0 == board[new_y][new_x] and 0 <= new_y < 8:
+            if 0 <= new_y < 8 and 8 > new_x >= 0 == board[new_y][new_x]:
                 possible_attacks_array[count_moves] = [new_y, new_x]
                 count_moves += 1
 
