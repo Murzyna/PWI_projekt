389bd9e29844450bde33ef2a7ce32835d2edcc26
Volqano <theriverpl@gmail.com>
2023-12-29 13:39:06 +0100

    -Teraz ataki figur działają za królem

--- szachy/game/pieces/bishop.py
+++ szachy/game/pieces/bishop.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pygame as pg
 from PWI_projekt.szachy.game.base_piece import *
+from PWI_projekt.szachy.game.pieces.king import *
 
 
 class Bishop(Piece):
@@ -44,7 +45,7 @@ class Bishop(Piece):
         directions = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
 
         max_possible_moves = 13  # Maksymalna liczba potencjalnych ruchów gońca
-        possible_moves_array = np.full((max_possible_moves, 2), -1, dtype=int)
+        possible_attacks_array = np.full((max_possible_moves, 2), -1, dtype=int)
 
         count_moves = 0
 
@@ -52,15 +53,19 @@ class Bishop(Piece):
             i, j = y + dy, x + dx
             while 0 <= i < 8 and 0 <= j < 8:
                 if board[i][j] == 0:
-                    possible_moves_array[count_moves] = [i, j]
+                    possible_attacks_array[count_moves] = [i, j]
                     count_moves += 1
 
                 else:
-                    possible_moves_array[count_moves] = [i, j]
-                    count_moves += 1
-                    break
+                    if isinstance(board[i][j], King) and board[i][j].color != self.color:
+                        possible_attacks_array[count_moves] = [i, j]
+                        count_moves += 1
+                    else:
+                        possible_attacks_array[count_moves] = [i, j]
+                        count_moves += 1
+                        break
                 i += dy
                 j += dx
 
         # Obcięcie tablicy do rzeczywistej liczby możliwych ruchów
-        self.possible_attacks = possible_moves_array[:count_moves]
+        self.possible_attacks = possible_attacks_array[:count_moves]
