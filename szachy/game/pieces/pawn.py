1e4677bd3b1c2d2e07bde3e217a6e65e02cf3d8d
JanAndrzejewski050 <janandrzejewski050@gmail.com>
2024-01-02 19:13:01 +0100

    working b2 pawn @

--- szachy/game/pieces/pawn.py
+++ szachy/game/pieces/pawn.py
@@ -36,7 +36,7 @@ class Pawn(Piece):
                 possible_moves_array[count_moves] = [y-1, x+1]
                 count_moves += 1
 
-            if 0<y<8 and 1<x<=8 and board[y-1][x-1] != 0 and board[y-1][x-1].color != self.color:       # bicie na ukos
+            if 0<y<8 and 0<x<=8 and board[y-1][x-1] != 0 and board[y-1][x-1].color != self.color:       # bicie na ukos
                 possible_moves_array[count_moves] = [y-1, x-1]
                 count_moves += 1
 
@@ -54,7 +54,7 @@ class Pawn(Piece):
                 possible_moves_array[count_moves] = [y+1, x+1]
                 count_moves += 1
 
-            if 0<y<8 and 1<x<=8 and board[y+1][x-1] != 0 and board[y+1][x-1].color != self.color:       # bicie na ukos
+            if 0<y<8 and 0<x<8 and board[y+1][x-1] != 0 and board[y+1][x-1].color != self.color:       # bicie na ukos
                 possible_moves_array[count_moves] = [y+1, x-1]
                 count_moves += 1
 
